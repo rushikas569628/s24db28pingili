24e8820509e62f3f839bbb79fe0fb4fc2f5a6ec8
rushikas569628 <reddyreducation@gmail.com>
2024-04-03 18:45:19 -0500

    post

--- controllers/hat.js
+++ controllers/hat.js
@@ -42,6 +42,27 @@ exports.hat_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    //Handle Costume create on POST.
+exports.hat_create_post = async function(req, res) {
+console.log(req.body)
+let document = new hat();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"costume_type":"goat", "cost":12, "size":"large"}
+document.hat_style = req.body.hat_style;
+document.price = req.body.price;
+document.size = req.body.size;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+
     
        
     
