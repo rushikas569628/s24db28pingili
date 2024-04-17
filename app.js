50d2aa668b1654c1a16e9228fbb6e6bf932c599d
rushikas569628 <reddyreducation@gmail.com>
2024-04-17 10:46:05 -0500

    modified buttons

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+ 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(
@@ -24,35 +24,26 @@ passport.use(new LocalStrategy(
   })
   })
   )
-
-require('dotenv').config();
-const connectionString = process.env.MONGO_CON;
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-
+ 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hatRouter = require('./routes/hat');
-var gridRouter = require('./routes/grid');
-var pickRouter = require('./routes/pick');
-var hatModel = require('./model/hat');
-var resourceRouter=require('./routes/resource');
+var gridrsRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick')
+var hat = require('./model/hat');
+var resourceRouter = require('./routes/resource')
+ 
 var app = express();
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+ 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+ 
 app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
@@ -60,17 +51,16 @@ app.use(require('express-session')({
   }));
   app.use(passport.initialize());
   app.use(passport.session());
-  
+ 
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hat', hatRouter);
-app.use('/grid', gridRouter);
-app.use('/pick', pickRouter);
-app.use('/hat',hatModel);
-app.use('/resource', resourceRouter);
-
+app.use('/hat',hatRouter);
+app.use('/grid',gridrsRouter);
+app.use('/pick',pickRouter);
+app.use ('/resource',resourceRouter)
+ 
 // passport config
 // Use the existing connection
 // The Account model
@@ -78,54 +68,67 @@ var Account =require('./model/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+ 
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+ 
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded");
+ 
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
+ 
 // We can seed the collection if needed on server start
 async function recreateDB(){
   // Delete everything
-  await hatModel.deleteMany();
-  let instance1 = new 
-  hatModel({hat_style:"Fedora", size:"Large", 
-  price:521});
+await hat.deleteMany();
+  let instance1 = new hat({hat_style:"Fedora", size:"Large", price:521});
   instance1.save().then(doc=>{
   console.log("First object saved")}
   ).catch(err=>{
   console.error(err)
   });
-  let instance2 = new 
-  hatModel({hat_style:"Boater hat", size:"Small", 
-  price:500});
+ 
+  let instance2 = new hat({hat_style:"Boater hat", size:"Small", price:500});
   instance2.save().then(doc=>{
   console.log("Second object saved")}
   ).catch(err=>{
   console.error(err)
   });
-  let instance3 = new 
-  hatModel({hat_style:"cowboy hat", size:"medium", 
-  price:590});
+ 
+  let instance3 = new hat({hat_style:"cowboy hat", size:"medium", price:590});
   instance3.save().then(doc=>{
   console.log("Third object saved")}
   ).catch(err=>{
   console.error(err)
   });
- }
- let reseed = true;
- if (reseed) {recreateDB();}
-
- module.exports = app;
+ 
+}
+ 
+  let reseed = true;
+  if (reseed) {recreateDB();}
+ 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+ 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+ 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+ 
+module.exports = app;
+ 
+ 
